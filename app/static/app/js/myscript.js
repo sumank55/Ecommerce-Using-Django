d9b2fd05cde6f1b5cd09c46a082d0b86f9651001
sumank55 <sumankumariideal@gmail.com>
2021-10-05 18:17:06 +0530

    addtocart

--- app/static/app/js/myscript.js
+++ app/static/app/js/myscript.js
@@ -21,3 +21,83 @@ $('#slider1, #slider2, #slider3').owlCarousel({
         }
     }
 })
+
+
+
+$('.plus-cart').click(function () {
+    console.log("Plus clicked")
+    var id = $(this).attr("pid").toString();
+    var em1=this.parentNode.children[2]
+    console.log("id is", id)
+    
+    $.ajax({
+        type: "GET",
+        url: "/pluscart",
+        data: {
+            prod_id: id
+
+        },
+        success: function (data){
+            // console.log("Data", data)
+            em1.innerText = data.quantity
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("totalamount").innerText=data.totalamount
+            
+        
+        }
+    })
+})
+
+
+
+$('.minus-cart').click(function () {
+    console.log("Minus clicked")
+    var id = $(this).attr("pid").toString();
+    var em1=this.parentNode.children[2]
+    console.log("id is", id)
+    
+    $.ajax({
+        type: "GET",
+        url: "/minuscart",
+        data: {
+            prod_id: id
+
+        },
+        success: function (data){
+            // console.log("Data", data)
+            em1.innerText = data.quantity
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("totalamount").innerText=data.totalamount
+            
+        
+        }
+    })
+})
+
+
+
+$('.remove-cart').click(function () {
+    console.log("Button clicked")
+    var id = $(this).attr("pid").toString();
+    var em1=this
+    console.log("id is", id)
+    
+    $.ajax({
+        type: "GET",
+        url: "/removecart",
+        data: {
+            prod_id: id
+
+        },
+        success: function (data){
+            // console.log("Data", data)
+        
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("totalamount").innerText = data.totalamount
+            em1.parentNode.parentNode.parentNode.parentNode.
+            remove()
+            
+        
+        }
+    })
+})
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 from .models import Customer,Product,Cart,OrderPlaced
 from .forms import CustomerRegistrationsForms,CustomerProfileForm
 from django.contrib import messages
+from django.db.models import Q
+from django.http import JsonResponse
 
 
 
@@ -22,10 +24,105 @@ class ProductDetailsView(View):
 
 
 def add_to_cart(request):
- return render(request, 'app/addtocart.html')
+    user=request.user
+    product_id=request.GET.get('prod_id')
+    product=Product.objects.get(id=product_id)
+    Cart(user=user,product=product).save()
+    # return render(request, 'app/addtocart.html')
+    return redirect('/cart')
+
+def show_cart(request):
+    if request.user.is_authenticated:
+        user=request.user
+        cart=Cart.objects.filter(user=user)
+        print("Cart",cart)
+        amount=0.0
+        shipping_amount = 70.0
+        total_amount = 0.0
+        cart_product = [ p for p in Cart.objects.all() if p.user==user]
+        print("cart_product",cart_product)
+
+        if cart_product:
+            for p in cart_product:
+                tempamount=(p.quantity * p.product.discounted_price)
+                amount += tempamount
+                totalamount = amount + shipping_amount
+            return render(request,'app/addtocart.html',{'carts':cart,'totalamount':totalamount,'amount':amount })
+        else:
+            return render(request,'app/emptycart.html')   
+
+
+
+def plus_cart(request):
+    if request.method == 'GET':
+        prod_id=request.GET['prod_id']
+        c=Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity+=1
+        c.save()
+        amount=0.0
+        shipping_amount=70.00
+        cart_product= [ p for p in Cart.objects.all() if p.user == request.user]
+        for p in cart_product:
+            tempamount=(p.quantity * p.product.discounted_price)
+            amount += tempamount
+            
+
+        data={
+            'quantity': c.quantity,
+            'amount': amount,
+            'totalamount': amount + shipping_amount
+            } 
+        print("Data",data)    
+        return JsonResponse(data)   
+
+def minus_cart(request):
+    if request.method == 'GET':
+        prod_id=request.GET['prod_id']
+        c=Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity-=1
+        c.save()
+        amount=0.0
+        shipping_amount=70.00
+        cart_product= [ p for p in Cart.objects.all() if p.user == request.user]
+        for p in cart_product:
+            tempamount=(p.quantity * p.product.discounted_price)
+            amount += tempamount
+            
+
+        data={
+            'quantity': c.quantity,
+            'amount': amount,
+            'totalamount': amount + shipping_amount
+            } 
+        print("Data",data)    
+        return JsonResponse(data)   
+
+def remove_cart(request):
+    if request.method == 'GET':
+        prod_id=request.GET['prod_id']
+        c=Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.delete()
+        amount=0.0
+        shipping_amount=70.00
+        cart_product= [ p for p in Cart.objects.all() if p.user == request.user]
+        for p in cart_product:
+            tempamount=(p.quantity * p.product.discounted_price)
+            amount += tempamount
+
+        data={
+            'amount': amount,
+            'totalamount': amount + shipping_amount
+            }   
+        return JsonResponse(data)   
+
+
+
+
+
+                    
 
 def buy_now(request):
- return render(request, 'app/buynow.html')
+    return render(request, 'app/buynow.html')
 
 
 def address(request):
@@ -68,7 +165,19 @@ class CustomerRegistrationView(View):
 
 
 def checkout(request):
- return render(request, 'app/checkout.html') 
+    user=request.user
+    add=Customer.objects.filter(user=user)
+    cart_items=Cart.objects.filter(user=user)
+    amount=0.00
+    shipping_amount=70.00
+    totalamount=0.00
+    cart_product= [ p for p in Cart.objects.all() if p.user == request.user]
+    if cart_product:
+        for p in cart_product:
+            tempamount=(p.quantity * p.product.discounted_price)
+            amount += tempamount
+            totalamount=amount+shipping_amount  
+    return render(request, 'app/checkout.html',{'add':add,'totalamount':totalamount})
 
 class ProfileView(View):
     def get(self,request):
